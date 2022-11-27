a4416cf5230873e5330e7a4b6f4216a0306a2208
Белочка Петунья <ego_dama@inbox.ru>
2022-11-27 16:41:31 +0300

    поменяла http на https

--- script.js
+++ script.js
@@ -188,7 +188,7 @@ const createCard = (cat, parent) => {
 }
 
 const showCat = (cat) => {
-    fetch(`http://sb-cats.herokuapp.com/api/2/${user}/show/${cat.id}`)
+    fetch(`https://sb-cats.herokuapp.com/api/2/${user}/show/${cat.id}`)
     .then(res => res.json())
     .then(data => {
         if(data.message === "ok") {
@@ -198,7 +198,7 @@ const showCat = (cat) => {
 }
 
 
-fetch(`http://sb-cats.herokuapp.com/api/2/${user}/show`)
+fetch(`https://sb-cats.herokuapp.com/api/2/${user}/show`)
     .then(res => res.json())
     .then(result => {
         if(result.message === "ok") {
@@ -211,7 +211,7 @@ fetch(`http://sb-cats.herokuapp.com/api/2/${user}/show`)
 
 
 const deleteCat = (id, tag) => {
-    fetch(`http://sb-cats.herokuapp.com/api/2/${user}/delete/${id}`, {
+    fetch(`https://sb-cats.herokuapp.com/api/2/${user}/delete/${id}`, {
         method: "DELETE"
     })
     .then(res => res.json())
@@ -224,7 +224,7 @@ const deleteCat = (id, tag) => {
 }
 
 const addCat = (cat) => {
-	fetch(`http://sb-cats.herokuapp.com/api/2/${user}/add`, {
+	fetch(`https://sb-cats.herokuapp.com/api/2/${user}/add`, {
 		method: "POST",
 		headers: { 
 			"Content-Type": "application/json"
@@ -258,7 +258,7 @@ addForm.addEventListener("submit", (e) => {
 });
 
 const updCat = (cat, id) => {
-	fetch(`http://sb-cats.herokuapp.com/api/2/${user}/update/${id}`, {
+	fetch(`https://sb-cats.herokuapp.com/api/2/${user}/update/${id}`, {
 		method: "PUT",
 		headers: {
 			"Content-Type": "application/json",
